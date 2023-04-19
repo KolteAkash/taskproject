c0e3b9450db6a9bc4f1e8f178acef12f291b1d2d
KolteAkash <akashkolte240899@gmail.com>
2023-04-19 10:53:08 +0530

    task service

---
+++ TaskService.java
@@ -0,0 +1,36 @@
+package com.example.task.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.task.model.Task;
+import com.example.task.repository.TaskRepository;
+
+@Service
+public class TaskService{
+	@Autowired 
+	TaskRepository taskrepository;
+	
+	
+public void addTask(Task task)	{
+	taskrepository.save(task);
+}
+
+public List<Task>getAllStatus(){
+	return taskrepository.findAll();
+}
+
+public Optional<Task>getStatusById(int task){
+	return taskrepository.findById(task);
+}
+	
+	
+	
+	
+	
+
+}
+
